3a3dd36949bd12799739c1ce45f3ada4b20ad14b
Wangyannenu <136248579+Wangyannenu@users.noreply.github.com>
2024-06-21 17:21:56 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def issue(run,clock,instructions,instructions_status,component_status,register_s
         component_status[0][2] = op
         component_status[0][3] = des
         component_status[0][5] = on2
+
         if on2 not in register_status:
             component_status[0][9] = 'yes'
         elif register_status[on2]:
@@ -57,7 +58,7 @@ def issue(run,clock,instructions,instructions_status,component_status,register_s
                 component_status[2][8] = 'yes'
 
             if register_status[on2]:
-                component_status[2][7] = register_status[on1]
+                component_status[2][7] = register_status[on2]
                 component_status[2][9] = 'no'
             elif register_status[on2] == '':
                 component_status[2][9] = 'yes'
@@ -74,7 +75,6 @@ def issue(run,clock,instructions,instructions_status,component_status,register_s
             if register_status[on1]:
                 component_status[3][6] = register_status[on1]
                 component_status[3][8] = 'no'
-                component_status[3]
             elif register_status[on1] == '':
                 component_status[3][8] = 'yes'
 
@@ -113,12 +113,12 @@ def read_operands(run,clock,instructions,instructions_status,component_status,re
     on2 = instructions[run][3]
 
     if op == 'load':
-        if component_status[0][9] == 'yes':
+        if (component_status[0][9] == 'yes' and component_status[0][8] == '') or \
+                (component_status[0][9] == '' and component_status[0][8] == 'yes') or \
+                (component_status[0][9] == '' and component_status[0][8] == '') or \
+                (component_status[0][9] == 'yes' and component_status[0][8] == 'yes'):
             instructions_status[run][1] = clock
-            # component_status[0][6] = ''
-            # component_status[0][7] = ''
-            # component_status[0][9] = 'yes'
-    # elif register_status[on2] == '' and on2 in register_status and register_status[on1] == '' and on1 in register_status:#表示可以读取操作数
+
     elif op == 'mult':
         if component_status[1][3] == des and component_status[1][4] == on1 and component_status[1][5] == on2:
             if component_status[1][8] == 'yes' and component_status[1][9] == 'yes':
@@ -166,11 +166,13 @@ def execute(run,clock,instrcutions,instrcutions_status,component_status,register
     on1 = instrcutions[run][2]
     on2 = instrcutions[run][3]
     if op == 'load' and exe_clock[run] == '':
-        exe_clock[run] = 1
+        exe_clock[run] = latetime[0]
+        # exe_clock[run] = 1
         component_status[0][9] = 'no'
 
     if op == 'mult'and exe_clock[run] == '':
-        exe_clock[run] = 10
+        exe_clock[run] = latetime[1]
+        # exe_clock[run] = 10
         if component_status[1][3] == des and component_status[1][4] == on1 and \
                 component_status[1][5] == on2:
             component_status[1][8] = 'no'
@@ -181,12 +183,14 @@ def execute(run,clock,instrcutions,instrcutions_status,component_status,register
             component_status[2][9] = 'no'
 
     if exe_clock[run] == '' and (op == 'add' or op == 'sub'):
-        exe_clock[run] = 2
+        exe_clock[run] = latetime[2]
+        # exe_clock[run] = 2
         component_status[3][8] = 'no'
         component_status[3][9] = 'no'
 
     if op == 'divd' and exe_clock[run] == '':
-        exe_clock[run] = 40
+        exe_clock[run] = latetime[3]
+        # exe_clock[run] = 40
         component_status[4][8] = 'no'
         component_status[4][9] = 'no'
 
@@ -196,23 +200,22 @@ def execute(run,clock,instrcutions,instrcutions_status,component_status,register
         instrcutions_status[run][2] = clock
 
 def write_back (run, clock, instructions, instructions_status, component_status, register_status) :
-    des = instructions[run][1]
     op = instructions[run][0]
+    des = instructions[run][1]
+    on1 = instructions[run][2]
+    on2 = instructions[run][3]
     pause = 0
     for i in range(run):
         if instructions_status[i][1] == 0 or instructions_status[i][1] == clock:
             if instructions[run][1] == instructions[i][2] or instructions[run][1] == instructions[i][3]:
                 pause = 1
 
-    # for i in range(len(instructions)):
-    #     if instructions_status[i][1] > instructions_status[run][0] and instructions_status[i][1] <= clock:
-    #         if instructions[i][2] == des or instructions[i][3] == des:
-    #             return  # 如果有冲突，则跳过这次写回，等待下一次机会
-
     if pause == 0:
         instructions_status[run][3] = clock
         for i in range(len(component_status)):
-            if op == component_status[i][2]:
+            if op == 'mult' and des == component_status[i][3] and on1 == component_status[i][4] and on2 == component_status[i][5]:
+                component_status[i] = [component_status[i][0]] + ['no']+['']*8
+            if op != 'mult' and des == component_status[i][3]:
                 component_status[i] = [component_status[i][0]] + ['no']+['']*8
         for i in range(5):
             if component_status[i][6] == register_status[des] and component_status[i][1] == 'yes':
@@ -244,8 +247,7 @@ def socreboard (instructions):
                         ["divide","no",'','','','','','','','']
                         ]
     register_status = {}
-    register_status = {
-        'F0': '', 'F2': '', 'F4': '', 'F6': '', 'F8': '', 'F10': ''}
+    register_status = {'F0': '', 'F2': '', 'F4': '', 'F6': '', 'F8': '', 'F10': ''}
     #用于标记指令是否被发射
     sign = np.zeros(len(instructions))
 
@@ -254,6 +256,7 @@ def socreboard (instructions):
     # print(component_status[0])
     # print(instructions)
     finish_all_instructions = 1
+    # print(len(instructions))
     while finish_all_instructions:
         #判断指令是否可以流出
         if instructions_count < len(instructions) and sign[instructions_count] == 0:
@@ -265,7 +268,6 @@ def socreboard (instructions):
             # print(on1)
             on2 = instructions[instructions_count][3]
             # print(on2)
-
             if op == 'load': #若确定load指令可以流出，在issue函数之中无需判断
                 if component_status[0][1] == 'no' and register_status[des] == '':
                     sign[instructions_count] = 1
@@ -299,10 +301,7 @@ def socreboard (instructions):
                     instructions_count += 1
                     # exe_clock = 40
 
-        # print(instructions_count)
-
         # run = 0#用于标记正在运行当中的指令
-
         for i in range(instructions_count):
             if sign[i] == 1:
                 if instructions_status[i][0] == 0:
@@ -313,7 +312,7 @@ def socreboard (instructions):
             if sign[i] == 1:
                 if instructions_status[i][0] == clock:
                     continue
-                if instructions_status[i][1] == 0 and instructions_status[i][0] != 0 :
+                if instructions_status[i][1] == 0 and instructions_status[i][0] != 0:
                     read_operands(i, clock, instructions, instructions_status, component_status, register_status)
 
         for i in range(instructions_count):
@@ -335,6 +334,7 @@ def socreboard (instructions):
                     #             stall = 1
                     # if stall == 0:
                     write_back(i, clock, instructions, instructions_status, component_status, register_status)
+
         print("cycle:",end='')
         print(clock)
         clock += 1
@@ -366,22 +366,33 @@ def socreboard (instructions):
         # 遍历值
         for value in table3.values():
             print("{:<10}".format(value), end=' ')
-
-
         print('\n')
+        if instructions_count == len(instructions):
+            for i in range(len(instructions)):
+                if instructions_status[i][3] == 0:
+                    finish_all_instructions = 1
+                    break
+                else:
+                    finish_all_instructions = 0
 
-        for i in range(len(instructions)-1):
-            if instructions_status[i][3] == 0:
-                finish_all_instructions = 1
-            else:
-                finish_all_instructions = 0
 if __name__ == "__main__":
+    latetime=[0,0,0,0]
+    print("请输入不同指令类型的延迟时间")
+    print("load指令:")
+    latetime[0] = int(input())
+    print("mult指令:")
+    latetime[1] = int(input())
+    print("add/sub指令:")
+    latetime[2] = int(input())
+    print("divd指令:")
+    latetime[3] = int(input())
     print("请输入要执行指令的条数：")
     n = int(input())
     print("请输入要执行的指令：")
-    ins = [['0']*4 + ['']]*n
+    # n=2
+    ins = [['0']*4]*n
     exe_clock = [''] * len(ins)
-    # ins = [['load','F6','34','R2',''],['load', 'F2','45+','R3',''],['mult','F0','F2','F6',''],['sub','F8','F6','F2',''],['divd','F10','F8','F6',''],['add','F6','F10', 'F2','']]
+    # ins = [ ['load', 'F2', 'R3', 'F8'],['add', 'F2', 'F6', 'F4']]
     for i in range(n):
         ins[i] = input().split(" ")
     socreboard(ins)
